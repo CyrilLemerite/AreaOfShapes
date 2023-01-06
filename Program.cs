7c19a6ed389aec0a5fa3f22e35b1fe2353b48aad
Cyril Uwagwu <cyril@lemeritegroupe.com>
2023-01-06 16:10:51 +0100

    Add Formula 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
 ﻿//This is a programe to compute the areas of shapes
+Console.Clear();
+Console.WriteLine("Computing Area Of a Triangle");
+Console.WriteLine("Area of Triangle A = 1/2 * b * h.");
+
+
+Console.ReadLine();
