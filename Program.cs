ed7cc4170348a75e5d5118d14debd5206037b7ce
Cyril Uwagwu <cyril@lemeritegroupe.com>
2023-01-06 17:08:41 +0100

    Add Class Shape

--- Program.cs
+++ Program.cs
@@ -1,7 +1,78 @@
 ﻿//This is a programe to compute the areas of shapes
+
+int option;
+Shape shape = new Shape();
+double area = 0;
+
 Console.Clear();
-Console.WriteLine("Computing Area Of a Triangle");
-Console.WriteLine("Area of Triangle A = 1/2 * b * h.");
+Console.WriteLine("***********************************");
+Console.WriteLine("Programe to compute area of shapes");
+Console.WriteLine("***********************************");
+Console.WriteLine();
+Console.WriteLine("Shape Area - Select a shape:");
+Console.WriteLine();
+
+Console.WriteLine("1. Area of Circle");
+Console.WriteLine("2. Area of Rectangle");
+Console.WriteLine("3. Area of Triangle");
+Console.WriteLine("4. Area of square");
+Console.WriteLine();
+
+Console.Write("Option: ");
+option = Convert.ToInt32(Console.ReadLine());
+
+switch (option)
+{
+    case 1:
+    area = shape.CircleArea();
+    break;
+}
+
+//Area area = new Area();
+//double answer = area.Rectangle(2,2);
 
 
-Console.ReadLine();
+
+
+
+public class Shape{
+    
+    public double CircleArea()
+    {
+
+        double aCircle = 0;
+   
+        return aCircle;
+    }
+
+    public double SquareArea()
+    {
+
+        double aSquare = 0;
+   
+        return aSquare;
+    }
+    
+    public double TriangleArea()
+    {
+
+        double aTriangle = 0;
+   
+        return aTriangle;
+    }
+
+    public double RectangleArea(double l, double b)
+    {
+        double length = l;
+        double breadth = b;
+        double aRectangle = 0;
+
+        aRectangle = length * breadth;
+
+        return aRectangle;
+    }
+
+}
+
+
+
